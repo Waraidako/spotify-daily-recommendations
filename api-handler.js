09573b6cc6a3160377b041d852d614900fdb7eea
Waraidako <Waraidako@users.noreply.github.com>
2023-10-18 02:51:23 +0300

    Version 0.4
    Added: checking if the "Recommendations playlist" already exists and repopulating it instead of making a new one

--- api-handler.js
+++ api-handler.js
@@ -15,10 +15,16 @@ async function fetchWebApi(endpoint, method, body) {
     return await res.json();
 }
 
+export async function getUserPlaylists() {
+    return (await fetchWebApi(
+        'v1/me/playlists', 'GET'
+    )).items;
+}
+
 async function getTopTracks(limit){
     // Endpoint reference : https://developer.spotify.com/documentation/web-api/reference/get-users-top-artists-and-tracks
     return (await fetchWebApi(
-        'v1/me/top/tracks?time_range=short_term&limit=' + limit.toString(), 'GET'
+        `v1/me/top/tracks?time_range=short_term&limit=${limit.toString()}`, 'GET'
     )).items;
 }
 
@@ -26,7 +32,7 @@ async function createPlaylist(tracksUri){
     const { id: user_id } = await fetchWebApi('v1/me', 'GET')
     const playlist = await fetchWebApi(
         `v1/users/${user_id}/playlists`, 'POST', {
-            "name": "Recommendation playlist",
+            "name": "Recommendations playlist",
             "description": "Daily/generated recommendations playlist",
             "public": false
         })
@@ -46,6 +52,35 @@ async function getRecommendations(topTracksIds, limit){
     )).tracks;
 }
 
+async function getSnapshot(playlistId) {
+    return (await fetchWebApi(`v1/playlists/${playlistId}`, 'GET')).snapshot_id;
+}
+
+async function refreshRecommendations(tracksUri, playlistId) {
+    const playlistTracks = (await fetchWebApi(`v1/playlists/${playlistId}/tracks`, "GET")).items;
+    let playlistTracksUri = [];
+    playlistTracks.forEach((track) => {
+        playlistTracksUri.push({"uri" : track.track.uri});
+    });
+    // await fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks`,  {
+    //     method: 'DELETE',
+    //     headers: {
+    //         Authorization: `Bearer ${await tokenHandler.getAccessTokenViaRefreshToken(clientId, clientSecret, localStorage.getItem("refreshToken"))}`,
+    //         "Content-Type": "application/json"
+    //     },
+    //     body: JSON.stringify({
+    //         "tracks": playlistTracksUri,
+    //         "snapshot_id": await getSnapshot(playlistId)
+    //     })
+    // });
+    await fetchWebApi(`v1/playlists/${playlistId}/tracks`, 'DELETE', {
+        "tracks": playlistTracksUri,
+        "snapshot_id": await getSnapshot(playlistId)
+    })
+    await fetchWebApi(`v1/playlists/${playlistId}/tracks`, 'POST', {
+        "uris": tracksUri
+    })
+}
 
 export async function generateRecommendations(amount) {
     log("generating " + amount.toString() + " tracks...");
@@ -55,13 +90,9 @@ export async function generateRecommendations(amount) {
     topTracks.forEach((track) => {
         topTracksIds.push(track.id);
     });
-    console.log(topTracksIds);
-    console.log(topTracksIds.slice(0, 5));
     const recommendedTracks = [];
     for (let i = 0; i < amount; i += 5) {
         const recommended = await getRecommendations(topTracksIds.slice(i, i + 5), 5);
-        console.log(typeof(recommended));
-        console.log(recommended);
         recommended.forEach((track) => {
             recommendedTracks.push(track);
         })
@@ -71,7 +102,19 @@ export async function generateRecommendations(amount) {
         tracksUri.push(track.uri);
     })
     log("fetched recommendations, adding to playlist...");
-    await createPlaylist(tracksUri);
+    const playlists = await getUserPlaylists();
+    let playlistPresent = "";
+    playlists.forEach((playlist) => {
+        if (playlist.name == "Recommendations playlist")
+            playlistPresent = playlist;
+    })
+    if (playlistPresent) {
+        log("found existing playlist, refreshing...");
+        await refreshRecommendations(tracksUri, playlistPresent.id);
+    } else {
+        log("creating the recommendations playlist...");
+        await createPlaylist(tracksUri);
+    }
     log("generated recommendations");
     return true;
 }
